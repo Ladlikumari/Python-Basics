0acd2e6046560edf91f349bb97ef9868ec5d0a0e
Ladli Kumari <lavikakumari18aug@gmail.com>
2026-07-15 14:12:19 +0530

    Task 1: Added Python Basics solution

---
+++ README.md
@@ -0,0 +1,11 @@
+# python basics
+Welcome to my python Basics repository.
+
+## About
+This repository contains my daily Python Practice question.
+
+## Goal
+- Practice python everyday
+- Improve Problem-solving skills
+- stay consistent
+
---
+++ Task1.py
@@ -0,0 +1,19 @@
+#Python program to find the largest of three numbers entered by the user.
+
+# Find the Largest of Three Numbers
+
+# Taking input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Finding the largest number
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+
+# Displaying the result
+print("The largest number is:", largest)

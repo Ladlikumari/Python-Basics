93c61230ba81560df284f245d97b2944f56daff1
Ladli Kumari <lavikakumari18aug@gmail.com>
2026-08-21 20:57:12 +0530

    Add Tasl18

---
+++ Task18.py
@@ -0,0 +1,14 @@
+"""Create a function greet(name) that prints:
+
+Hello, Ladli!
+Welcome to Python.
+
+for the input "Ladli"."""
+
+
+def greet(name):
+    print("Hello,", name + "!")
+    print("Welcome to Python.")
+
+
+greet("Ladli")
